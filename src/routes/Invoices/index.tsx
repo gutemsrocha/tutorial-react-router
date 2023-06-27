1bfc881e2d817c67d7832f58ea1da620c29e8fff
Gutembergue Junior <gutemsrocha@gmail.com>
2023-06-27 19:27:42 -0300

    Custom behavior

--- src/routes/Invoices/index.tsx
+++ src/routes/Invoices/index.tsx
@@ -1,11 +1,12 @@
 import './styles.css';
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet, useSearchParams } from "react-router-dom";
 import { getInvoices } from "../../data";
+import QueryLink from '../../components/QueryLink';
 
 export default function Invoices() {
 
   const invoices = getInvoices();
-
+  const [searchParams, setSearchParams] = useSearchParams();
 
   return (
     <div style={{ display: "flex" }}>
@@ -15,15 +16,37 @@ export default function Invoices() {
           padding: "1rem",
         }}
       >
-        {invoices.map((invoice) => (
-          <NavLink
-            className={({ isActive }) => isActive ? "dblock nav-red" : "dblock nav-blue"}
-            to={`/invoices/${invoice.number}`}
-            key={invoice.number}
-          >
-            {invoice.name}
-          </NavLink>
-        ))}
+        <input
+          value={searchParams.get("name") || ""}
+          onChange={(event) => {
+            const name = event.target.value;
+            if (name) {
+              setSearchParams({ name });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
+
+        {invoices
+          .filter((invoice) => {
+            const name = searchParams.get("name");
+            if (!name) {
+              return true;
+            }
+            const invoiceName = invoice.name.toLowerCase();
+            return invoiceName.startsWith(name.toLowerCase());
+          })
+          .map((invoice) => (
+            <QueryLink
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              className={({ isActive }: any) => isActive ? "dblock nav-red" : "dblock nav-blue"}
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </QueryLink>
+          ))}
       </nav>
       <Outlet />
     </div>
